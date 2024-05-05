04d9beca88e5ab6d643bef1488d83f34e1ded109
Aaron Fihn <35422728+aaronfihn@users.noreply.github.com>
2024-05-05 09:17:29 -0400

    Remove hard-coded Jersey references

--- src/main/java/com/example/jaxrstesting/JaxRsConfig.java
+++ src/main/java/com/example/jaxrstesting/JaxRsConfig.java
@@ -1,15 +1,16 @@
 package com.example.jaxrstesting;
 
 import jakarta.ws.rs.ApplicationPath;
-import org.glassfish.jersey.server.ResourceConfig;
+import java.util.*;
 import org.springframework.stereotype.Component;
 
 @Component
 @ApplicationPath("/api/v1")
-public class JaxRsConfig extends ResourceConfig {
-
-  public JaxRsConfig() {
-    register( DemoController.class );
+public class JaxRsConfig extends jakarta.ws.rs.core.Application {
+  @Override
+  public Set<Class<?>> getClasses() {
+    Set<Class<?>> resources = new HashSet<>();
+    resources.add(DemoController.class);
+    return resources;
   }
-
 }
